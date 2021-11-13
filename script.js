7379309d52bcb9ceb36880cb6d79c9aed6d881a5
mariaassisf <90994184+mariaassisf@users.noreply.github.com>
2021-11-13 20:43:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+alert("olá esse e um pequeno sistema de banco")
+let saldo = parseInt(prompt("digite o seu saldo"))
+let compra = parseInt(prompt("digite o valor de sua compra"))
+
+if(saldo >= compra){
+    saldo = saldo - compra
+    alert("compra realizada")
+}
+else if(saldo >= compra){
+    saldo = saldo - compra 
+    alert("compra realizada")
+    }
+    else{
+        alert("não foi possivel realizar a compra")
+    }
